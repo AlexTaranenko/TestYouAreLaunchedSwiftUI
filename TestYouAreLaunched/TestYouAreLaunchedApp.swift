46e57b38467757bc6ee3abf33f09e85a44f15142
alexandertaranenko <alexander.taranenko@mova.io>
2023-04-23 21:52:32 +0300

    Initial Commit

---
+++ TestYouAreLaunched/TestYouAreLaunchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestYouAreLaunchedApp.swift
+//  TestYouAreLaunched
+//
+//  Created by Alex on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestYouAreLaunchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
